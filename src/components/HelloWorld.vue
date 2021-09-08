c43a7cc57ca1b4451f709cd0bacba61868bf611a
huishun98 <huishun98@gmail.com>
2021-09-08 23:21:23 +0800

    added loader

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="container fixed-width">
-    <h1>URL shortening service</h1>
-    <p>(like [bit.ly](https://bit.ly))</p>
-    <b-form-input v-model="url" placeholder="Enter URL"></b-form-input>
-    <p v-show="shortenedUrl.length > 0" class="remarks margin-top-10">
-      Your shortened url is: {{ shortenedUrl }}
-    </p>
-    <b-button variant="outline-primary" class="margin-top-10" @click="submit"
-      >Shorten</b-button
-    >
+  <div>
+    <div class="overlay" v-show="loading">
+      <b-spinner class="spinner" variant="primary"></b-spinner>
+    </div>
+    <div class="container fixed-width">
+      <h1>URL shortening service</h1>
+      <p>(like [bit.ly](https://bit.ly))</p>
+      <b-form-input v-model="url" placeholder="Enter URL"></b-form-input>
+      <p v-show="shortenedUrl.length > 0" class="remarks margin-top-10">
+        Your shortened url is: {{ shortenedUrl }}
+      </p>
+      <b-button variant="outline-primary" class="margin-top-10" @click="submit"
+        >Shorten</b-button
+      >
+    </div>
   </div>
 </template>
 
@@ -22,6 +27,7 @@ export default {
     return {
       url: "",
       shortenedUrl: "",
+      loading: false,
     };
   },
   methods: {
@@ -34,16 +40,21 @@ export default {
         this.url.substring(0, 7) == "http://" ||
         this.url.substring(0, 8) == "https://"
       ) {
+        this.loading = true;
         axios
           .post(backendUrl, {
             url: this.url,
           })
           .then((res) => {
             this.shortenedUrl = backendUrl + res.data;
+            this.loading = false;
+          })
+          .catch(() => {
+            this.loading = false;
           });
         return;
       }
-      alert("Please enter a valid URL");
+      alert("Please enter a valid URL starting with http:// or https://");
     },
   },
 };
@@ -56,5 +67,18 @@ export default {
 }
 .margin-top-10 {
   margin-top: 10px;
+}
+.spinner {
+  display: block;
+  margin: 20px auto;
+}
+.overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.6);
+  padding-top: 100px;
 }
 </style>
